b269e8528e34fe9a48da1832952953bca1b65c57
ICANTAIM <91802789+ICANTAIM@users.noreply.github.com>
2021-10-30 14:39:04 -0400

    added tutorial thingy

--- main.ts
+++ main.ts
@@ -2,6 +2,14 @@ let Boolet = 0
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Player_1.vy = -200
 })
+function Tutorial () {
+    game.splash("Welcome to the Night Fight Tutorial")
+    game.splash("To move left and right, press left and right")
+    game.splash("Press A (Z) to jump")
+    game.splash("If playing with 2 players,")
+    game.splash("P1: wasd + q = A e = B")
+    game.splash("P2: ijkl + i = A o = B")
+}
 sprites.onOverlap(SpriteKind.Player, Boolet, function (sprite, otherSprite) {
     sprite.setVelocity(0, 0)
     music.pewPew.play()
@@ -149,6 +157,7 @@ Player_1 = sprites.create(img`
     . . . . . 6 . . . 6 . . . . . . 
     `, SpriteKind.Player)
 info.setLife(3)
+scene.cameraFollowSprite(Player_1)
 tiles.setTilemap(tilemap`level1`)
 controller.moveSprite(Player_1, 100, 0)
 Player_1.ay = 500
